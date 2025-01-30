cf6dd939efcdb32fbc183ad7f245308224cf18fe
Juan Simancas <dev.juansimancas@proton.me>
2025-01-29 22:16:45 -0500

    add solutions for traits 00, 01, 02 and 04

--- exercises/04_traits/02_orphan_rule/src/lib.rs
+++ exercises/04_traits/02_orphan_rule/src/lib.rs
@@ -3,9 +3,10 @@
 //  a foreign type (`u32`, from `std`).
 //  Look at the compiler error to get familiar with what it looks like.
 //  Then delete the code below and move on to the next exercise.
+struct New32(u32);
 
-impl PartialEq for u32 {
+impl PartialEq for New32 {
     fn eq(&self, _other: &Self) -> bool {
-        todo!()
+        self.0 == _other.0
     }
 }
