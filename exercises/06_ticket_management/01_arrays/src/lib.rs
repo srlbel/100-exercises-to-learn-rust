c792f6dd6de4530c424699ea00af4fc10b0e6773
Juan Simancas <dev.juansimancas@proton.me>
2025-03-07 11:26:02 -0500

    add exercise 06 - 01, 02

--- exercises/06_ticket_management/01_arrays/src/lib.rs
+++ exercises/06_ticket_management/01_arrays/src/lib.rs
@@ -1,7 +1,7 @@
 // TODO: Flesh out the `WeekTemperatures` struct and its method implementations to pass the tests.
 
 pub struct WeekTemperatures {
-    // TODO
+    temperatures: [Option<i32>; 7],
 }
 
 pub enum Weekday {
@@ -16,15 +16,37 @@ pub enum Weekday {
 
 impl WeekTemperatures {
     pub fn new() -> Self {
-        todo!()
+        WeekTemperatures {
+            temperatures: [None; 7],
+        }
     }
 
     pub fn get_temperature(&self, day: Weekday) -> Option<i32> {
-        todo!()
+        let index: usize = match day {
+            Weekday::Monday => 0,
+            Weekday::Thursday => 1,
+            Weekday::Wednesday => 2,
+            Weekday::Tuesday => 3,
+            Weekday::Friday => 4,
+            Weekday::Saturday => 5,
+            Weekday::Sunday => 6,
+        };
+
+        self.temperatures[index]
     }
 
     pub fn set_temperature(&mut self, day: Weekday, temperature: i32) {
-        todo!()
+        let index: usize = match day {
+            Weekday::Monday => 0,
+            Weekday::Thursday => 1,
+            Weekday::Wednesday => 2,
+            Weekday::Tuesday => 3,
+            Weekday::Friday => 4,
+            Weekday::Saturday => 5,
+            Weekday::Sunday => 6,
+        };
+
+        self.temperatures[index] = Some(temperature)
     }
 }
 
